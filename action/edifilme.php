0cdc308a2a22a4b9aae06d82023fa42321a1612d
JulianoCP <juliano-petini@hotmail.com>
2018-06-14 15:53:40 -0300

    Arrumando o Editar Filme

--- action/edifilme.php
+++ action/edifilme.php
@@ -11,10 +11,11 @@ $nome_genero = "";
 ?>
 <?php  include '../conexao.php';?>
 <div class="container">
-<form class="borda" action='updfilme.php' method='get'>
-  <div class='row justify-content-center'>
+  <form class="borda" action='updfilme.php' method='get'>
+    <div class='col-md-12 justify-content-center'>
+      <div class='bord row'>
       <figure class='figure'>
-<?php $sql = "SELECT F.NOME AS FILMENOME, F.ANO AS ANOFILME, F.DURACAO AS DURAFILME,
+        <?php $sql = "SELECT F.NOME AS FILMENOME, F.ANO AS ANOFILME, F.DURACAO AS DURAFILME,
         F.IMG, F.SINOPSE,F.DIRETOR,F.ATOR, C.NOMECAT AS CATNOME,FF.idFormato as FORID,I.idIdioma AS IDIOID, I.NOME AS IDIFILME,C.idCategoria AS CATEID,R.NOME AS RESUFILME,R.idresolucao AS RESUID, FF.NOME AS FFNOME
         FROM FILMES AS F, IDIOMA AS I,RESOLUCAO AS R,CATEGORIA AS C,FORMATO AS FF
         WHERE F.idFilmes = '{$_GET['id']}'
@@ -23,176 +24,158 @@ $nome_genero = "";
         AND FF.idFormato = F.Formato_idFormato
         AND R.idresolucao = F.resolucao_idresolucao";
 
-  $result = $conn->query($sql);
-  if ($result->num_rows > 0) {
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
 
-    while($row = $result->fetch_assoc()) {
-      $si=$row['SINOPSE'];
-      $id_resolucao=$row['RESUID'];
-      $nome_resolucao=$row['RESUFILME'];
-      $id_formato=$row['FORID'];
-      $nome_formato=$row['FFNOME'];
-      $id_idioma=$row['IDIOID'];
-      $nome_idioma=$row['IDIFILME'];
-      $id_genero=$row['CATEID'];
-      $nome_genero=$row['CATNOME'];
+          while($row = $result->fetch_assoc()) {
+            $si=$row['SINOPSE'];
+            $id_resolucao=$row['RESUID'];
+            $nome_resolucao=$row['RESUFILME'];
+            $id_formato=$row['FORID'];
+            $nome_formato=$row['FFNOME'];
+            $id_idioma=$row['IDIOID'];
+            $nome_idioma=$row['IDIFILME'];
+            $id_genero=$row['CATEID'];
+            $nome_genero=$row['CATNOME'];
 
-      echo "
-            <img src='../src/img/logo/".$row["IMG"]."' class='figure-img img-fluid rounded' alt='' height='500' width='1500>
+            echo "
+            <img src='".$row["IMG"]."' class='figure-img img-fluid rounded' alt=''>
             <figcaption class='figure-caption'></figcaption>
-          </figure>
-          <div class='col'>
-              <h5 class='text'></h5>
+            </figure>
+
+            <div class='add3 form-group col-md-4'>
+            <label for='inputAddress'><span style='color:red;'>Nome do Filme</span></label>
+            <input type='text' class='form-control' id='nome' name='nome' placeholder='Nome...' value='{$row['FILMENOME']}'/>
+            <input type='hidden' id='id' name='id' value='{$_GET['id']}'/>
+
+            <label for='inputAddress'><span style='color:red;'>Ano de Lamçamento</span></label>
+            <input type='text' class='form-control' id='ano' name='ano' placeholder='Ano...' value='{$row['ANOFILME']}'/>
+
+            <label for='inputAddress'><span style='color:red;'>Duração do Filme</span></label>
+            <input type='text' class='form-control' id='duracao' name='duracao' placeholder='Duração...' value='{$row['DURAFILME']}'/>
+
+            <label for='inputAddress'><span style='color:red;'>Diretor do Filme</span></label>
+            <input type='text' class='form-control' id='diretor' name='diretor' placeholder='Diretor...' value='{$row['DIRETOR']}'/>
+
+            <label for='inputAddress'><span style='color:red;'>Ator Principal do Filme</span></label>
+            <input type='text' class='form-control' id='ator' name='ator' placeholder='Ator...' value='{$row['ATOR']}'/>
+            </div>
+            <div class='add3 form-group col-md-4'>
+            <label for='inputAddress'><span style='color:red;'>Formato do Filme</span></label>";
+
+            ?>
+            <select id="formato" class="genero form-control" name="formato">
+              <?php
+              echo "<option value=". $id_formato .">". $nome_formato ."</option>";
+              $sql = "SELECT Nome,idFormato FROM formato";
+              $result = $conn->query($sql);
+              if ($result->num_rows > 0) {
+                while($row = $result->fetch_assoc()) {
+                  if($row["idFormato"] != $id_formato){
+                    echo "<option value=". $row["idFormato"] .">". $row["Nome"] ."</option>";
+                  }
+                }
+              } else {
+                echo "0 results";
+              }
+              ?>
+            </select>
+
+            <?php
+
+            echo"
+
+
+            <label for='inputAddress'><span style='color:red;'>Resolucao do Filme</span></label>
+            <select id='resolucao' class='genero form-control' name='resolucao'>
+            <option value=". $id_resolucao .">". $nome_resolucao ."</option>";
+            ?>
+            <?php
+            $sql = "SELECT Nome,idresolucao FROM resolucao";
+            $result = $conn->query($sql);
+            if ($result->num_rows > 0) {
+              while($row = $result->fetch_assoc()) {
+                if( $row["idresolucao"] != $id_resolucao){
+                  echo "<option value=". $row["idresolucao"] .">". $row["Nome"] ."</option>";
+                }
+              }
+            } else {
+              echo "0 results";
+            }
+            ?>
+          </select>
+
+          <?php
+
+          echo"
+
+          <label for='inputAddress'><span style='color:red;'>Idioma do Filme</span></label>";
+
+          ?>
+
+          <select id="idioma" class="genero form-control" name="idioma">
+            <?php
+            echo "<option value=". $id_idioma .">". $nome_idioma ."</option>";
+            $sql = "SELECT Nome,idIdioma FROM idioma";
+            $result = $conn->query($sql);
+            if ($result->num_rows > 0) {
+              while($row = $result->fetch_assoc()) {
+                if($row["idIdioma"] != $id_idioma){
+                  echo "
+                  <option value=". $row["idIdioma"] .">". $row["Nome"] ."</option>
+                  ";
+                }
+              }
+            } else {
+              echo "0 results";
+            }
+            ?>
+          </select>
+
+          <?php
+
+          echo"<label for='inputState'><span style='color:red;'>Gênero</span></label>";
+
+          ?>
+
+          <select id="categoria" class="genero form-control" name="categoria">
+            <?php
+            echo "<option value=". $id_genero .">". $nome_genero ."</option>";
+            $sql = "SELECT NomeCat,idcategoria FROM categoria";
+            $result = $conn->query($sql);
+            if ($result->num_rows > 0) {
+              while($row = $result->fetch_assoc()) {
+                if ($row["idcategoria"] != $id_genero){
+                  echo "
+                  <option value=". $row["idcategoria"] .">". $row["NomeCat"] ."</option>
+                  ";
+                }
+              }
+            } else {
+              echo "0 results";
+            }
+            ?>
+          </select>
+
+          <?php
+
+          echo"
+
+          <label for='inputState'><span style='color:red;'>Sinopse</span></label>
+          <input type='text' class='form-control' id='sinopse' name='sinopse' placeholder='Sinopse...' value='{$si}'/>
           </div>
-        </div>
-        <div class=' form-row'>
-            <div class='form-group col-md-4'>
-              <label for='inputAddress'><span style='color:red;'>Nome do Filme</span></label>
-              <input type='text' class='form-control' id='nome' name='nome' placeholder='Nome...' value='{$row['FILMENOME']}'/>
-              <input type='hidden' id='id' name='id' value='{$_GET['id']}'/>
-            </div>
-            <div class='form-group col-md-4'>
-              <label for='inputAddress'><span style='color:red;''>Ano de Lamçamento</span></label>
-              <input type='text' class='form-control' id='ano' name='ano' placeholder='Ano...' value='{$row['ANOFILME']}'/>
-            </div>
-            <div class='form-group col-md-4'>
-              <label for='inputAddress'><span style='color:red;'>Duração do Filme</span></label>
-              <input type='text' class='form-control' id='duracao' name='duracao' placeholder='Duração...' value='{$row['DURAFILME']}'/>
-            </div>
-            <div class='form-group col-md-4'>
-              <label for='inputAddress'><span style='color:red;'>Diretor do Filme</span></label>
-              <input type='text' class='form-control' id='diretor' name='diretor' placeholder='Diretor...' value='{$row['DIRETOR']}'/>
-            </div>
-            <div class='form-group col-md-4'>
-              <label for='inputAddress'><span style='color:red;'>Ator Principal do Filme</span></label>
-              <input type='text' class='form-control' id='ator' name='ator' placeholder='Ator...' value='{$row['ATOR']}'/>
-            </div>
-            <div class='form-group col-md-4'>
-              <label for='inputAddress'><span style='color:red;'>Formato do Filme</span></label>";
-
-              ?>
-              <select id="formato" class="genero form-control" name="formato">
-                <?php
-                echo "<option value=". $id_formato .">". $nome_formato ."</option>";
-                $sql = "SELECT Nome,idFormato FROM formato";
-                $result = $conn->query($sql);
-                if ($result->num_rows > 0) {
-                  while($row = $result->fetch_assoc()) {
-                    if($row["idFormato"] != $id_formato){
-                      echo "<option value=". $row["idFormato"] .">". $row["Nome"] ."</option>";
-                    }
-                  }
-                } else {
-                  echo "0 results";
-                }
-                ?>
-              </select>
-
-                <?php
-
-                 echo"
-
-            </div>
-            <div class='form-group col-md-4'>
-              <label for='inputAddress'><span style='color:red;'>Resolucao do Filme</span></label>
-              <select id='resolucao' class='genero form-control' name='resolucao'>
-              <option value=". $id_resolucao .">". $nome_resolucao ."</option>";
-              ?>
-                <?php
-                $sql = "SELECT Nome,idresolucao FROM resolucao";
-                $result = $conn->query($sql);
-                if ($result->num_rows > 0) {
-                  while($row = $result->fetch_assoc()) {
-                    if( $row["idresolucao"] != $id_resolucao){
-                      echo "<option value=". $row["idresolucao"] .">". $row["Nome"] ."</option>";
-                    }
-                  }
-                } else {
-                  echo "0 results";
-                }
-                ?>
-              </select>
-
-                <?php
-
-                 echo"
-
-            </div>
-
-
-            <div class='form-group col-md-4'>
-              <label for='inputAddress'><span style='color:red;'>Idioma do Filme</span></label>";
-
-              ?>
-
-              <select id="idioma" class="genero form-control" name="idioma">
-                <?php
-                echo "<option value=". $id_idioma .">". $nome_idioma ."</option>";
-                $sql = "SELECT Nome,idIdioma FROM idioma";
-                $result = $conn->query($sql);
-                if ($result->num_rows > 0) {
-                  while($row = $result->fetch_assoc()) {
-                    if($row["idIdioma"] != $id_idioma){
-                    echo "
-                    <option value=". $row["idIdioma"] .">". $row["Nome"] ."</option>
-                    ";
-                   }
-                  }
-                } else {
-                  echo "0 results";
-                }
-                ?>
-              </select>
-
-                <?php
-
-                 echo"
-
-            </div>
-            <div class='form-group col-md-4'>
-              <label for='inputState'><span style='color:red;'>Gênero</span></label>";
-
-              ?>
-
-              <select id="categoria" class="genero form-control" name="categoria">
-                <?php
-                echo "<option value=". $id_genero .">". $nome_genero ."</option>";
-                $sql = "SELECT NomeCat,idcategoria FROM categoria";
-                $result = $conn->query($sql);
-                if ($result->num_rows > 0) {
-                  while($row = $result->fetch_assoc()) {
-                    if ($row["idcategoria"] != $id_genero){
-                    echo "
-                    <option value=". $row["idcategoria"] .">". $row["NomeCat"] ."</option>
-                    ";
-                   }
-                  }
-                } else {
-                  echo "0 results";
-                }
-                ?>
-              </select>
-
-                <?php
-
-                 echo"
-
-            </div>
-            <div class='form-group col-md-12'>
-              <label for='inputState'><span style='color:red;'>Sinopse</span></label>
-              <input type='text' class='form-control' id='sinopse' name='sinopse' placeholder='Sinopse...' value='{$si}'/>
-
-            </div>
           </div>
-      ";
-    }
-  } else {
-    echo "0 results";
-  }
-  ?>
-  <div class="text-center">
-    <button type="submit" class="btn btn-outline-danger">ATUALIZAR</button>
+          </div>
+          ";
+        }
+      } else {
+        echo "0 results";
+      }
+      ?>
+      <div class="text-center">
+        <button type="submit" class="btn btn-outline-danger" style="margin-top:25px;">ATUALIZAR</button>
+      </div>
+    </div>
+    </form>
   </div>
-  </form>
-</div>
-<?php include("../paginas/rodape.php") ?>
+  <?php include("../paginas/rodape.php") ?>
